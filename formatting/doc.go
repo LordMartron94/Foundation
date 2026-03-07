a3e9ae38b9a3e9baf81b45260a93622638544efa
Matthew van der Hoorn <md.career@protonmail.com>
2026-03-07 10:21:14 +0100

    byte formatter

--- formatting/doc.go
+++ formatting/doc.go
@@ -1,2 +1,3 @@
-// Package formatting provides functionality for formatting different structures.
+// Package formatting provides functionality for formatting different structures,
+// including memory sizes (e.g. byte, KiB, MiB) for error messages and diagnostics.
 package formatting
